0ddf425791d0f29674fa48798dca1c0058ba12fd
kurocon <kevin@kevinalberts.nl>
2014-05-06 14:35:23 +0200

    Rewrite rule 1

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,7 +1,4 @@
-SELECT p.pid FROM Person p
-WHERE EXISTS(
-SELECT * FROM Acts a, Movie m
+SELECT p.pid FROM Person p, Acts a, Movie m
 WHERE a.pid = p.pid
 AND m.mid = a.mid
-AND m.name = 'Back to the Future'
-);
+AND m.name = 'Back to the Future';
