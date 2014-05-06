39d97186d240b2eccc487e8802c50ec61a2215e8
kurocon <kevin@kevinalberts.nl>
2014-05-06 14:44:24 +0200

    Exercise 3c, stap 3

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -2,5 +2,9 @@ SELECT p.name FROM Person p
 WHERE EXISTS(
 SELECT * FROM Writes w
 WHERE w.pid = p.pid
-AND -- movie without director
+AND EXISTS(
+SELECT * FROM Movie m
+WHERE m.mid = w.mid
+AND -- no director
+)
 );
