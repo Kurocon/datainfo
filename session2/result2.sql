f9be94ffc86ad3b24d4933dc660891240dbbfa40
kurocon <kevin@kevinalberts.nl>
2014-05-06 14:13:23 +0200

    Session 2, Exercise 2b

--- session2/result2.sql
+++ session2/result2.sql
@@ -1,3 +1,8 @@
-INSERT INTO F VALUES(“ABC”, “EF”, “true”, “Geen triviale FD”)
-INSERT INTO F VALUES(“E”, “D”, “true”, “Geen superkey”)
-INSERT INTO F VALUES(“D”, “A”, “true”, “Geen superkey”)
+INSERT INTO F VALUES("ABC", "EF", TRUE, "Geen triviale FD")
+INSERT INTO F VALUES("E", "D", TRUE, "Geen superkey")
+INSERT INTO F VALUES("D", "A", TRUE, "Geen superkey")
+
+INSERT INTO F1 VALUES("ABC", "E", FALSE, "")
+INSERT INTO F2 VALUES("ABC", "F", FALSE, "")
+INSERT INTO F2 VALUES("E", "D", TRUE, "Geen superkey")
+INSERT INTO F2 VALUES("D", "A", TRUE, "Geen superkey")
