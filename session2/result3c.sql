466be708afe1590ac0b6ce1f94344ce61d9f26c9
kurocon <kevin@kevinalberts.nl>
2014-05-06 14:59:30 +0200

    Exercise 3c, stap 8

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,11 +1,5 @@
 SELECT p.name FROM Person p
-WHERE p.pid IN (
-	SELECT w.pid
-	FROM Writes w, Movie m, Directs d
-	WHERE w.mid IN (
-		SELECT m.mid
-		FROM Movie m, Directs d)
-	AND NOT w.mid IN(
-		SELECT d.mid
-		FROM Directs d)
-);
+WHERE p.pid IN(
+	SELECT w.pid FROM Writes w
+	WHERE w.mid NOT IN(
+		SELECT d.mid FROM Directs d));
