9fa0e7016fd1c80bc640e0e661047b398ab04cc1
kurocon <kevin@kevinalberts.nl>
2014-05-06 14:13:02 +0200

    Exercise 1 fix

--- session2/result1.sql
+++ session2/result1.sql
@@ -1,10 +1,11 @@
-INSERT INTO Problem1 VALUES(“a”, TRUE, “#3 impliceert dat C de capaciteit is van een voertuig V”);
+INSERT INTO Problem1 VALUES(“a”, FALSE, “Meerdere V’s kunnen capaciteit C hebben.”);
 INSERT INTO Problem1 VALUES(“b”, TRUE, “als je het voertuig hebt kan je zijn capaciteit opzoeken met #3”);
-INSERT INTO Problem1 VALUES(“c”, TRUE, “uit #8 blijkt dat F vermeld welke P erbij hoort”);
+INSERT INTO Problem1 VALUES(“c”, FALSE, “Een F kan meerdere P’s hebben”);
 INSERT INTO Problem1 VALUES(“d”, TRUE, “#7: A weet welke P erbij hoort en #8: F weet welke P erbij hoort”);
 INSERT INTO Problem1 VALUES(“e”, TRUE, “#5: G weet welke P erbij hoort en #6: O weet welke P erbij hoort”);
 INSERT INTO Problem1 VALUES(“f”, FALSE, “een B kan meerdere V’s hebben”);
 INSERT INTO Problem1 VALUES(“g”, FALSE, “er kunnen meerdere B’s zijn voor V”);
-INSERT INTO Problem1 VALUES(“h”, TRUE, “als je alle V’s weet weet je alle B’s (#2)”);
-INSERT INTO Problem1 VALUES(“i”, TRUE, “als je alle V’s weet weet je alle C’s (#3)”);
-INSERT INTO Problem1 VALUES(“j”, TRUE, “h) en i) zijn ook allebei waar.”);
+
+INSERT INTO Problem1 VALUES(“h”, TRUE, “B heeft geen relaties met CPGOAF”);
+INSERT INTO Problem1 VALUES(“i”, TRUE, “C heeft geen relaties met BPGOAF”);
+INSERT INTO Problem1 VALUES(“j”, TRUE, “B en C hebben geen relaties met PGOAF.”);
